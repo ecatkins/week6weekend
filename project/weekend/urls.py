8a1d58e049fd404bb7878104b5d45263839769b1
ecatkins <ecatkins@gmail.com>
2015-08-01 18:02:44 -0400

    Some HTML

--- project/weekend/urls.py
+++ project/weekend/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from weekend.views import IndexView, InstagramView, FrontView
 urlpatterns = [
-    url(r'^$', FrontView.as_view(),name='front')
+    url(r'^$', FrontView.as_view(),name='front'),
     url(r'^gay_pride$', IndexView.as_view(), name='index'),
     url(r'^instagram/(?P<search>[\w]*)/(?P<interval>[\w]*)$', InstagramView.as_view(), name='instagram'),
     url(r'^flag$', 'weekend.views.flag', name='flag'),
